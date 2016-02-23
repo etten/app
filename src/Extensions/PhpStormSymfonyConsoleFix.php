95e3477f08ac6156c0601f1cc9b11f818a377342
Jaroslav Hranička <hranicka@outlook.com>
2016-02-23 18:34:01 +0100

    Extensions\PhpStormSymfonyConsoleFix fixed

--- src/Extensions/PhpStormSymfonyConsoleFix.php
+++ src/Extensions/PhpStormSymfonyConsoleFix.php
@@ -12,8 +12,12 @@ class PhpStormSymfonyConsoleFix implements App\AppExtension
 	{
 		// PhpStorm & Symfony Console hotfix
 		// See https://youtrack.jetbrains.com/issue/WI-29627
-		if (isset($argv[1]) && $argv[1] === '-V') {
-			die('Symfony version 2.8.0');
+
+		if (php_sapi_name() === 'cli') {
+			$argv = $_SERVER['argv'];
+			if (isset($argv[1]) && $argv[1] === '-V') {
+				die('Symfony version 2.8.0');
+			}
 		}
 	}
 

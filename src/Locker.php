784293c5e036c1a0d75b732fa5ec8eb028b854b1
Jaroslav Hranička <hranicka@outlook.com>
2016-03-04 16:00:00 +0100

    Locker: Output command result

--- src/Locker.php
+++ src/Locker.php
@@ -26,6 +26,7 @@ class Locker
 	public function lock()
 	{
 		if (!$this->isLocked()) {
+			echo "App Locked.\n";
 			file_put_contents($this->lockFile, NULL);
 		}
 	}
@@ -33,6 +34,7 @@ class Locker
 	public function unlock()
 	{
 		if ($this->isLocked()) {
+			echo "App Unlocked.\n";
 			unlink($this->lockFile);
 		}
 	}

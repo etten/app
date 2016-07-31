ff8bae1d80a19d68d660bc9de9571b70c38cd84e
hranicka <hranicka@outlook.com>
2016-07-31 12:46:31 +0200

    Correctly set umask, mb_internal_encoding

--- src/Extensions/SystemSetup.php
+++ src/Extensions/SystemSetup.php
@@ -2,12 +2,10 @@
 
 namespace Etten\App\Extensions;
 
-use Nette;
-
 class SystemSetup extends AbstractExtension
 {
 
-	public function onConfiguratorCreate(Nette\Configurator $configurator, array $config)
+	public function onAppLoad(array $config)
 	{
 		umask(0); // 0666 file, 0777 folder
 		mb_internal_encoding('UTF-8'); // forces UTF-8 internal encoding
